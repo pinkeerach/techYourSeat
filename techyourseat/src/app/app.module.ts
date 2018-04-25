08f258e502df73eace1ff9141e211b4f2e84f2a3
Jocelyn Keung <jocelyn.keung@captechventures.com>
2018-04-25 00:14:23 -0400

    Adding organizer images and links to org sites

--- techyourseat/src/app/app.module.ts
+++ techyourseat/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SponsorsComponent } from './sponsors/sponsors.component';
 import { AboutComponent } from './about/about.component';
+import { OrganizersComponent } from './organizers/organizers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AboutComponent,
-    SponsorsComponent
+    SponsorsComponent,
+    OrganizersComponent
   ],
   imports: [BrowserModule, BrowserAnimationsModule, SharedModule],
   providers: [],
